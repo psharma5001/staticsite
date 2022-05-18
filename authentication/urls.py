9c4f0a677979e8af0fbebdac600401a1313d70a8
Piyush Sharma <60728460+psharma5001@users.noreply.github.com>
2022-05-18 17:20:44 +0530

    update

---
+++ authentication/urls.py
@@ -0,0 +1,12 @@
+# import khud krlena
+from django.urls import path
+from .views import *
+from django.contrib.auth.views import LogoutView
+
+app_name ="authen"
+urlpatterns = [
+    path('signin/', login_view, name="login"),
+    path('signup/', register_user, name="register"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    
+]
--- web/urls.py
+++ web/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('',index, name="index"),
+    path('signup/', register_user, name="register"),
+    path('signin/', login_view, name="login"),
 ]
